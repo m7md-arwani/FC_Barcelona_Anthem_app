1e58f7496c8b43f9d39487ca69e8a70aafd75f87
m7md-arwani <moh.3ara@hotmail.com>
2021-06-01 09:20:07 +0300

    Adding the App files

---
+++ app/src/main/java/com/example/android/audio_test/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.android.audio_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1,b2,b3,b4 ;
+        MediaPlayer fcb = MediaPlayer.create(this, R.raw.bb) ;
+
+        
+b1 = (Button) findViewById(R.id.play_button) ;
+
+b1.setOnClickListener(new View.OnClickListener() {
+
+    @Override
+    public void onClick(View view) {
+        fcb.start();
+        fcb.setOnCompletionListener(mp -> {
+            Toast.makeText(getApplicationContext(),"Finished!", Toast.LENGTH_SHORT).show() ;
+            fcb.release();
+
+        });
+    }
+}) ;
+
+        b2 = (Button) findViewById(R.id.pause_button) ;
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                fcb.pause();
+            }
+        }) ;
+
+        AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        b3 = (Button) findViewById(R.id.up_button) ;
+
+        b3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+            }
+        }) ;
+
+        b4 = (Button) findViewById(R.id.down_button) ;
+
+        b4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
+            }
+        }) ;
+
+
+    }
+
+
+
+}
